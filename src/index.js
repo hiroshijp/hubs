ba1ec28a20a02e5b8c17a38e027cc6d169ba2ef5
hiroshijp <hiroshijp0616@gmail.com>
2024-09-19 17:30:29 +0900

    [update] hcce-observerへのPOSTを追加

--- src/index.js
+++ src/index.js
@@ -28,3 +28,24 @@ function HomeRoot() {
 const container = document.getElementById("home-root");
 const root = createRoot(container);
 root.render(<HomeRoot />);
+
+// post access info to hubs-observer
+var email = "anonymous";
+var from = window.location.href;
+const localStorageData = localStorage.getItem("___hubs_store");
+if (localStorageData) {
+    email = JSON.parse(localStorageData).credentials.email;
+} 
+
+const req = {
+ visitor: { mail: email },
+ visited_from: from,
+};
+
+fetch('https://hcce-observer-955595017457.us-central1.run.app/visited', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(req)
+})
